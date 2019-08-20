4130d6bd6c23ec69e256a3d5f9909aa322ef51b2
Raul Ortiz <raulortizars@gmail.com>
2019-08-19 19:16:24 -0600

    progress i guess

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,29 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
+//data?
+date = 'March 28, 2019';
+
+// date span, class
+
+function dateSpanner (date) {
+    const dateSpan = document.createElement('span')
+    dateSpan.classList.add('date')
+    dateSpan.textContent = date
+
+    return dateSpan
+}
+
+// h1
+function h1er(h1) {
+    const titleH1 =document.createElement('h1')
+    titleH1.textContent = h1
+
+    return titleH1
+}
+
+// temp span
+
+
+//main comp, everything appended to it
 function Header() {}
